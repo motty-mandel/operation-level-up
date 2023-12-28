a8ee60a7c24c33afbd9d2e4a69250f4a6278f246
motty mandel <mandelmottyisrael65@gmail.com>
2023-12-27 20:17:51 -0500

    started on the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './footer.css';
+
 export default function Footer() {
     return (
-        <div className="navbar fixed-bottom navbar-light bg-light">
-            <h1>Footer</h1>
+        <div className="navbar fixed-bottom">
+            <div className="row">
+                <div className="col-12">
+                    <p className="copyright">&copy; 2021</p>
+                </div>
+            </div>
         </div>
     );
 }
