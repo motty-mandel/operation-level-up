de8c46f2837e5ca14916fe66b5cbfab5f0863a62
motty mandel <mandelmottyisrael65@gmail.com>
2024-02-25 10:50:51 -0500

    finally figured out .env

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URL);
 
 module.exports = mongoose.connection;
